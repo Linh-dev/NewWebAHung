738ee76a25f59e91de5943f45544c9c15954b448
linhld <linhld@newwave.vn>
2022-06-03 16:53:08 +0700

    ..

--- eFashionShop/Migrations/20220602063519_11.Designer.cs
+++ eFashionShop/Migrations/20220602063519_11.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eFashionShop.Data.EF;
 
@@ -11,9 +12,10 @@ using eFashionShop.Data.EF;
 namespace eFashionShop.Migrations
 {
     [DbContext(typeof(EShopDbContext))]
-    partial class EShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220602063519_11")]
+    partial class _11
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

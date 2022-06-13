5d7a5058ffa5e847d998d5e55b0e6f61e17ac587
LinhLD <linhld@dft.vn>
2022-06-13 12:17:08 +0700

    update ss

---
+++ eFashionShop/Controllers/AdminController/AdminImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eFashionShop.Controllers.AdminController
+{
+    public class AdminImageController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
